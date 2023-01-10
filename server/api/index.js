5115acc3304dd891d424ecf9340828ddabeeefd7
Sean Brown <seanbrown5757@gmail.com>
2023-01-10 15:21:41 -0500

    deleted cart route on index.js

--- server/api/index.js
+++ server/api/index.js
@@ -3,8 +3,6 @@ module.exports = router;
 
 router.use("/products", require("./products"));
 
-router.use("/cart", require("./cart"));
-
 router.use("/users", require("./users"));
 
 router.use((req, res, next) => {
