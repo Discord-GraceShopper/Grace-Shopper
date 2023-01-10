4b5f525240109b7ece29f86ed78380c79b247b4c
Sean Brown <seanbrown5757@gmail.com>
2023-01-10 13:04:22 -0500

    added single product

--- server/api/products.js
+++ server/api/products.js
@@ -4,9 +4,19 @@ const {
 } = require("../db");
 module.exports = router;
 
+// Get all products
 router.get("/", async (req, res, next) => {
   try {
     res.json(await Product.findAll());
+  } catch (err) {
+    next(err);
+  }
+});
+
+//Get single product
+router.get("/:id", async (req, res, next) => {
+  try {
+    res.json(await Product.findByPk(req.params.id));
   } catch (err) {
     next(err);
   }
