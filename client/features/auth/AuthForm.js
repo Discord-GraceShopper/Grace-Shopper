99507e9e1fb829b7046d21ebfdf056cc3075899d
Sean Brown <seanbrown5757@gmail.com>
2023-01-11 15:35:00 -0500

    required inputs on all fields

--- client/features/auth/AuthForm.js
+++ client/features/auth/AuthForm.js
@@ -46,25 +46,25 @@ const AuthForm = ({ name, displayName }) => {
               <label htmlFor="first name">
                 <small>First Name</small>
               </label>
-              <input name="first_name" type="text" />
+              <input required name="first_name" type="text" />
             </div>
             <div>
               <label htmlFor="last name">
                 <small>Last Name</small>
               </label>
-              <input name="last_name" type="text" />
+              <input required name="last_name" type="text" />
             </div>
             <div>
               <label htmlFor="email">
                 <small>Email</small>
               </label>
-              <input name="email" type="text" />
+              <input required name="email" type="text" />
             </div>
             <div>
               <label htmlFor="password">
                 <small>Password</small>
               </label>
-              <input name="password" type="text" />
+              <input required name="password" type="text" />
             </div>
             <div>
               <button type="submit">{displayName}</button>
@@ -80,13 +80,13 @@ const AuthForm = ({ name, displayName }) => {
               <label htmlFor="email">
                 <small>Email</small>
               </label>
-              <input name="email" type="text" />
+              <input required name="email" type="text" />
             </div>
             <div>
               <label htmlFor="password">
                 <small>Password</small>
               </label>
-              <input name="password" type="password" />
+              <input required name="password" type="password" />
             </div>
             <div>
               <button type="submit">{displayName}</button>
