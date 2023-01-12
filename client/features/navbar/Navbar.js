a2821e9b3717c217413aa9be4ac90398da2ab1ba
Sean Brown <seanbrown5757@gmail.com>
2023-01-12 17:38:31 -0500

    style changes for navbar

--- client/features/navbar/Navbar.js
+++ client/features/navbar/Navbar.js
@@ -17,7 +17,7 @@ const Navbar = () => {
       <h1>Grace Shopper</h1>
       <nav>
         {isLoggedIn ? (
-          <div>
+          <div className="navbar">
             {/* The navbar will show these links after you log in */}
             <Link to="/">Home</Link>
             <Link to="/profile">Account</Link>
@@ -26,7 +26,7 @@ const Navbar = () => {
             </button>
           </div>
         ) : (
-          <div>
+          <div className="navbar">
             {/* The navbar will show these links before you log in */}
             <Link to="/">Home</Link>
             <Link to="/login">Login/Register</Link>
