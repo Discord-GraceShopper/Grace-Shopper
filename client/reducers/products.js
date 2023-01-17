d5aa5d2dfbd9759e5a6985304b03f871230bf654
Michael Timo <mtimo744@gmail.com>
2023-01-16 19:21:29 -0500

    added thunks, thunk.fulfilled for each

--- client/reducers/products.js
+++ client/reducers/products.js
@@ -10,6 +10,35 @@ export const getAllProducts = createAsyncThunk("products/getAll", async () => {
   }
 });
 
+export const createNewProduct = createAsyncThunk(
+  "products/create", 
+  async ({ title, brand, sku, description, price, availability, quantity, primary_category }) => {
+  try {
+    const { data } = await axios.post('/api/products', {
+      title,
+      brand,
+      sku,
+      description,
+      price,
+      availability,
+      quantity,
+      primary_category,
+    })
+      return data;
+  } catch (error) {
+    return error;
+  }
+})
+
+export const deleteProduct = createAsyncThunk("products/delete", async (productId) => {
+  try {
+    const { data } = await axios.delete(`/api/products/${productId}`);
+    return data;
+  } catch (error) {
+    return error;
+  }
+})
+
 const initialState = {
   allProducts: [],
   error: null,
@@ -26,7 +55,16 @@ export const productSlice = createSlice({
       })
       .addCase(getAllProducts.rejected, (state, action) => {
         state.allProducts = action.error;
-      });
+      })
+      .addCase(createNewProduct.fulfilled, (state, action) => {
+        state.allProducts.push(action.payload);
+      }) // What to do with createNewProduct.rejected?
+      .addCase(deleteProduct.fulfilled, (state, action) => {
+        const productToDelete = action.payload;
+        const indexFinder = (product) => product.id === productToDelete.id;
+        const indexOfProduct = state.allProducts.findIndex(indexFinder);
+        state.splice(indexOfProduct, 1);
+      }) // What to do with deleteProduct.rejected?
   },
 });
 
