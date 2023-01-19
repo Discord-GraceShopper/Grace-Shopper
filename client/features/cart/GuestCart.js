8a7ca307c08897b280c3437d80d1ad3899dea172
Justin Suh <justindjsuh@gmail.com>
2023-01-19 01:48:26 -0500

    refactored the html format to copy styling from user cart

--- client/features/cart/GuestCart.js
+++ client/features/cart/GuestCart.js
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
+import { editQuantity, deleteItem } from "../../reducers/cart";
 
 const GuestCart = () => {
   const [cart, setCart] = useState(
@@ -10,14 +11,32 @@ const GuestCart = () => {
     setCart(JSON.parse(localStorage.getItem("cart")) || []);
   }, [cart.length]);
 
+  /*
+   * addQty and subtractQty don't work it needs to be adjusted
+   * to handle guest cart info as of this moment, I don't see a way
+   * to access orderId since it doesn't exist for guests
+   */
   const addQty = (product) => {
-    product.productQuantity += 1;
+    let productId = product.order_details.productId;
+    let orderId = product.order_details.orderId;
+    let item_quantity = product.order_details.item_quantity;
+    let price = product.price;
+    item_quantity++;
+    const updatedQty = { id, productId, orderId, item_quantity, price };
+    if (item_quantity <= product.quantity) dispatch(editQuantity(updatedQty));
   };
 
   const subtractQty = (product) => {
-    product.productQuantity -= 1;
-    if (product.productQuantity < 1) {
-      deleteItems(product);
+    let productId = product.order_details.productId;
+    let orderId = product.order_details.orderId;
+    let item_quantity = product.order_details.item_quantity;
+    let price = product.price;
+    if (item_quantity === 1) {
+      dispatch(deleteItem({ productId, orderId }));
+    } else {
+      item_quantity--;
+      const updatedQty = { id, productId, orderId, item_quantity, price };
+      dispatch(editQuantity(updatedQty));
     }
   };
 
@@ -36,43 +55,61 @@ const GuestCart = () => {
   let subTotal = Number(0);
 
   return (
-    <div>
-      <h1>Cart</h1>
+    <div className="cart-container">
+      <h1 className="cart-header">Guest Cart</h1>
       <div>
         {cart &&
           cart.map((product) => {
             subTotal += Number(product.productPrice);
             return (
-              <div key={product.id}>
-                <h2>{product.productTitle}</h2>
+              <div key={product.productId} className="cart-details">
                 <Link to={`/products/${product.productId}`}>
-                  <img src={product.productImg} width="200" height="200" />
+                  <div className="cart-image">
+                    <img src={product.productImg} width="200" height="200" />
+                  </div>
                 </Link>
-                <h3>{product.productPrice}</h3>
-                <h3>Qty: {product.productQuantity}</h3>
-                <div>
-                  <button id="remove" onClick={() => deleteItems(product)}>
-                    X
+                <h2 className="cart-product-details">{product.productTitle}</h2>
+                <h3 className="cart-product-details">
+                  ${product.productPrice}
+                </h3>
+                <h3 className="cart-product-details">
+                  Qty:{" "}
+                  <button
+                    className="cart-subtract-qty"
+                    onClick={() => subtractQty(product)}
+                  >
+                    -
                   </button>
-                  <button id="add" onClick={() => addQty(product)}>
+                  {product.productQuantity}
+                  <button
+                    className="cart-add-qty"
+                    onClick={() => addQty(product)}
+                  >
                     +
                   </button>
-                  <button id="subtract" onClick={() => subtractQty(product)}>
-                    -
+                </h3>
+                <div>
+                  <button
+                    className="cart-remove-button"
+                    onClick={() => deleteItems(product)}
+                  >
+                    X
                   </button>
                 </div>
               </div>
             );
           })}
-        <h1>Order Summary</h1>
-        <h3>{`Subtotal: ${subTotal.toFixed(2)}`} </h3>
-        <h3>Delivery: </h3>
-        <h3>{`Tax: ${(subTotal * 0.08).toFixed(2)}`}</h3>
-        <h3>{`Total: ${(subTotal + subTotal * 0.08).toFixed(2)}`}</h3>
-        <div>
-          <Link to="/checkout">
-            <button>Proceed to Checkout</button>
-          </Link>
+        <div className="ordersummary-container">
+          <h1>Order Summary</h1>
+          <h3>{`Subtotal: $${subTotal.toFixed(2)}`} </h3>
+          <h3>Delivery: </h3>
+          <h3>{`Tax: $${(subTotal * 0.08).toFixed(2)}`}</h3>
+          <h3>{`Total: $${(subTotal + subTotal * 0.08).toFixed(2)}`}</h3>
+          <div>
+            <Link to="/checkout">
+              <button className="checkout-button">Proceed to Checkout</button>
+            </Link>
+          </div>
         </div>
       </div>
     </div>
